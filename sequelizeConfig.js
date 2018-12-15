19a631f7d01c0ab1f005a83dfc78e4e3800fefdf
Rehan <rajpootrehan25@gmail.com>
2018-12-15 15:14:31 +0500

    set pkg json

--- sequelizeConfig.js
+++ sequelizeConfig.js
@@ -1,9 +1,9 @@
 var Sequelize = require('sequelize');
 
 // sequelize = new Sequelize('database Name','database username','database pass',{
-sequelize = new Sequelize('SMS','root','',{
+sequelize = new Sequelize('id7989057_smsapi','id7989057_rehan102','Rehan@123',{
 	// host : 'host name',
-	host : 'localhost',
+	host : 'https://databases-auth.000webhost.com',
 	port : 3306,
 	dialect : 'mysql',
 	dialectOptions : { 
